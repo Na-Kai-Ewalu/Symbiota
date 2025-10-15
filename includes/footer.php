1e879474ed73acc70998cce0403cdd1b7ee74e26
Katie Pearson <katelin.d.pearson24@gmail.com>
2025-10-15 15:02:45 -0700

    Modifications from feedback

--- includes/footer.php
+++ includes/footer.php
@@ -6,6 +6,7 @@
 		<a href="https://kuahawaii.org/" target="_blank" title="<?= "KUA" ?>" aria-label="<?= "KUA" ?>">
 			<img src="<?= $CLIENT_ROOT; ?>/images/layout/kua-logo.png"  alt="<?= "KUA Logo" ?>" />
 		</a>
+		<!--
 		<a href="https://www.hawaii.edu/" target="_blank" title="<?= "University of Hawaii" ?>" aria-label="<?= "University of Hawaii" ?>">
 			<img src="<?= $CLIENT_ROOT; ?>/images/layout/uni-hawaii.png"  alt="<?= "University of Hawaii Seal" ?>" />
 		</a>
@@ -15,6 +16,7 @@
 		<a href="https://www.berkeley.edu/" target="_blank" title="<?= "University of California, Berkeley" ?>" aria-label="<?= "University of California, Berkeley" ?>">
 			<img src="<?= $CLIENT_ROOT; ?>/images/layout/berkeley.png"  alt="<?= "UC Berkeley Logo" ?>" />
 		</a>
+		-->
 		<img />
 		<a href="https://biodiversity.ku.edu" target="_blank" title="<?= $LANG['F_KU-BI'] ?>" aria-label="<?= $LANG['F_KU-BI'] ?>">
 			<img src="<?= $CLIENT_ROOT; ?>/images/layout/ku-bi_logo.png"  alt="<?= $LANG['F_KU-BI_LOGO'] ?>" />
